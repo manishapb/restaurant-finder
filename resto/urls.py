4503e2f094df14d200342b4842e79e60b4e1aa6b
Manisha Pillai <pillaimanisha77@gmail.com>
2018-05-10 17:27:01 +0530

    Remove unwanted url

--- resto/urls.py
+++ resto/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import url
 app_name = 'resto'
 
 urlpatterns = [
-    url(r'^create/', RestoCreateView.as_view(), name='create'),
     url(r'^list/', ListRestoView.as_view(), name = 'list'),
     url(r'^(?P<pk>\d+)/$', DetailRestoView.as_view(), name='resto_detail'),
     url(r'^search/', search, name='search'),
