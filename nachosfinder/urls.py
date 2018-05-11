eca61071bb5c6f4e791001b0b5219a6a47429266
Manisha Pillai <pillaimanisha77@gmail.com>
2018-05-11 16:51:33 +0530

    Update ui

--- nachosfinder/urls.py
+++ nachosfinder/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_page, name='home'),
     url(r'^resto/', include('resto.urls',namespace='resto')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
