688ddbde69322a0a58b307543b9eaa446661c5f3
Manisha Pillai <pillaimanisha77@gmail.com>
2018-06-08 10:41:21 +0530

    Change lat long

--- csvtopostg.py
+++ csvtopostg.py
@@ -18,7 +18,7 @@ with open(fname,newline='\n') as cf:
 	for row in reader:
 		print(row['lat'],row['lng'])
 		name = row['name'].replace("'","")
-		query_str = "INSERT INTO resto_resto(name, address) VALUES ('{}', ST_GeomFromText('POINT({} {})',4326))".format(name,row['lat'],row['lng']);
+		query_str = "INSERT INTO resto_resto(name, address) VALUES ('{}', ST_GeomFromText('POINT({} {})',4326))".format(name,row['lng'],row['lat']);
 		print(query_str)
 		cur.execute(query_str)
 conn.commit()
